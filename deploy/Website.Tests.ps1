1c436ade4ec4393c8dacd799ce747f86aee0c622
blastbeats <andrew.dotson2@gmail.coiim>
2025-06-08 16:58:36 -0400

    added smoke test job

---
+++ deploy/Website.Tests.ps1
@@ -0,0 +1,21 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidationNotNullOrEmpty()]
+    [string] $HostName
+)
+
+Describe 'Toy Website' {
+    It 'Serves pages over HTTPS' {
+        $request =  [System.Net.HttpWebRequest]::Create("https://$HostName/")
+        $request.AllowAuthRedirect = $false
+        $request.GetResponse().StatusCode |
+            Should -Be 200 -Because "the website requires HTTPS"
+    }
+
+    It 'Does not serve pages over HTTP' {
+        $request = [System.Net.HttpWebRequest]::Create("http://$HostName/")
+        $request.AllowAuthRedirect = $false
+        $request.GetResponse().StatusCode | 
+            Should -BeGreaterOrEqual 300 -Because "HTTP is not secure"
+    }
+}
